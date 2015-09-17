7e46ce89800ce1ee3785965f104444961e8585d8
Derek <deepsky_5@live.com>
2015-09-17 22:53:21 +0000

    testing acts_as_votable gem

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,10 +1,9 @@
 class Artwork < ActiveRecord::Base
+  acts_as_votable
   belongs_to :user
 	has_many :comments
 	belongs_to :category
 	
-
-	acts_as_votable
 	has_attached_file :image, styles: {medium: "300x300#", large: "600x500>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-   root 'artworks#index'
+     get 'artworks/purchase'=> 'artworks#purchase'
+     root 'artworks#index'
    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
