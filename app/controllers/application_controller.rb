5591cfd2b0c4c667c350820dbec4135f04901fd5
Derek <deepsky_5@live.com>
2015-09-22 12:28:22 +0000

    edit application and index.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,17 +20,17 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     if current_user.artist?
-    root_path
-    else
     artworks_path
+    else
+    root_path
     end
   end
 
   def after_sign_in_path_for(resource)
     if current_user.artist?
-      root_path
+      artworks_path
     else
-    artworks_path
+      root_path
     end
   end
 
