81bcb9ee26278ff6b7fde993b5d50ff01cfcf341
Derek <deepsky_5@live.com>
2015-09-24 06:36:44 +0000

    add validations to artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -7,4 +7,15 @@ class Artwork < ActiveRecord::Base
 	
 	has_attached_file :image, styles: {medium: "300x300#", large: "600x500>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  validates :caption, presence: true,
+            length: {minimum: 3}
+
+  validates :description, presence: true,
+            length: {minimum: 10}
+
+  validates :image, presence: true
+
+  validates :category_id, presence: true
+
 end
