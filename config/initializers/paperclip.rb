c96caa49424e8068d2c7ec1ba2ea5e6edf31195d
Derek <deepsky_5@live.com>
2015-09-30 16:46:27 +0000

    configuring s3 storage for afrikart

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,12 @@
-# paperclip_defaults = Rails.application.config_for :paperclip
-# paperclip_defaults.symbolize_keys!
+paperclip_defaults = Rails.application.config_for :paperclip
+paperclip_defaults.symbolize_keys!
 
-# Paperclip::Attachment.default_options.merge! paperclip_defaults
+Paperclip::Attachment.default_options.merge! paperclip_defaults
+
+production: &production
+    storage: :s3
+    bucket:  <%= ENV["S3_BUCKET_NAME"] %>
+    url:     ":afrikart.s3-eu-west-1.amazonaws.com"
+    path:    ":class/:attachment/:id_partition/:style/:filename.:extension"
+
+staging: *production
