45506711a969ed126a37a1cdcd9c5fc1ea438111
Derek <deepsky_5@live.com>
2015-10-02 17:59:14 +0000

    resolve s3 issue

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,4 @@
 # paperclip_defaults.symbolize_keys!
 
 # Paperclip::Attachment.default_options.merge! paperclip_defaults
+Paperclip::Attachment.default_options[:s3_host_name] = 'afrikart.s3-eu-west-1.amazonaws.com/'
